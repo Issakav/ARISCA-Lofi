86a09ffe63a2ae4408930e5d30a1357441c87376
Issakav <svanthul@macalester.edu>
2023-02-22 11:28:37 -0600

    tried to get audio to play

--- ourScript.js
+++ ourScript.js
@@ -1,4 +1,44 @@
-function myFunction() {
-    document.getElementById("demo").innerHTML = "Paragraph changed.";
-    document.write("hi");
-}
+
+const AudioContext = window.AudioContext || window.webKitAudioContext;
+
+const audioContext = new AudioContext();
+
+// get the audio element
+const audioElement = document.querySelector("audio");
+
+// pass it into the audio context
+const track = audioContext.createMediaElementSource(audioElement);
+
+track.connect(audioContext.destination);
+
+
+const playButton = document.querySelector("button");
+
+playButton.addEventListener(
+  "click",
+  () => {
+    // Check if context is in suspended state (autoplay policy)
+    if (audioContext.state === "suspended") {
+      audioContext.resume();
+    }
+
+    // Play or pause track depending on state
+    if (playButton.dataset.playing === "false") {
+      audioElement.play();
+      playButton.dataset.playing = "true";
+    } else if (playButton.dataset.playing === "true") {
+      audioElement.pause();
+      playButton.dataset.playing = "false";
+    }
+  },
+  false
+);
+
+audioElement.addEventListener(
+    "ended",
+    () => {
+      playButton.dataset.playing = "false";
+    },
+    false
+  );
+  
