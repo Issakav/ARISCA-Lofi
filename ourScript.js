6989d9907892f7dfed10c7aee10eba84788447b8
Issakav <svanthul@macalester.edu>
2023-03-16 18:03:36 -0500

    all audio tracks added and each has their own volume node

--- ourScript.js
+++ ourScript.js
@@ -2,13 +2,14 @@ console.log("hello");
 
 let audioContext;
 let volume;
+let gainNodes = [];
 let tracks; //Drums, Piano, Melody, Guitar
 const startBtn =  document.querySelector(".start");
 const setupBtn = document.querySelector(".setupTracks");
 const playBtn = document.querySelector(".playSample");
 
 
-const oneBar = 6000; // length of one bar. TODO: update
+const oneBar = 5647; // length of one bar. TODO: update
 /* Note: The new sounds that I added in are all 5.647 second long. 
 I know this is a really inconvenient number and if the math doesn't work or anything
 lmk and I can fix the length, that was just the length that all of them looped
@@ -16,14 +17,17 @@ the best when I listened to it on garageband
 */
 let nextTime = 0;
 
+const drumTrackPaths = ["./audio/Drum_1.wav","./audio/Drum_2.wav","./audio/Drum_3.wav", "./audio/Drum_4.wav","./audio/Drum_5.wav"];
+const guitarTrackPaths = ["./audio/Guitar_1.wav","./audio/Guitar_2.wav","./audio/Guitar_3.wav","./audio/Guitar_4.wav","./audio/Guitar_5.wav"]
+const melodyTrackPaths = ["./audio/Melody_1.wav","./audio/Melody_2.wav","./audio/Melody_3.wav","./audio/Melody_4.wav","./audio/Melody_5.wav"]
+const pianoTrackPaths = ["./audio/Piano_1.wav","./audio/Piano_2.wav","./audio/Piano_3.wav","./audio/Piano_4.wav","./audio/Piano_5.wav"]
 
-
-const trackPaths = ["./audio/drum_loop_6.wav","./audio/top_loop_7.wav","./audio/melody_3_old.wav","./audio/melody_5_old.wav"]; //update to hold path names for all audio files
+const trackPaths = drumTrackPaths.concat(guitarTrackPaths).concat(melodyTrackPaths).concat(pianoTrackPaths);
 //TODO fix idea: set all loops to the same audio, around 10 times and see if the delay is still present
 startBtn.addEventListener("click", () => {
   audioContext = new AudioContext();
-  volume = audioContext.createGain();
-  volume.connect(audioContext.destination);
+  // volume = audioContext.createGain();
+  // volume.connect(audioContext.destination);
   console.log("Started the Audio Context");
 });
 
@@ -36,7 +40,7 @@ setupBtn.addEventListener("click", () => {
       const playingTracks = [];
       for (const track of tracks) {
         nextTime = audioContext.currentTime;
-        playingTracks.push(playTrack(track, 0).start(0));
+        playingTracks.push(playTrack(track, 0).start());
       }
       //check setTimout and setInterval to switch tracks every x loops. Check if there is something like this in the webAudioAPI.
     });
@@ -70,6 +74,9 @@ async function setupTracks(paths) {
 function playTrack(audioBuffer, time){
   const trackSource = audioContext.createBufferSource();
   trackSource.buffer = audioBuffer;
+  volume = audioContext.createGain();
+  volume.connect(audioContext.destination);
+  gainNodes.push(volume);
   trackSource.connect(volume);
   trackSource.loop = true;
   //trackSource.start(time); //TODO: moving this to playingTracks.push(playTrack(track, 0).start(0)); in the playBtn event listener solved the issue so that all tracks now start at the same time.
